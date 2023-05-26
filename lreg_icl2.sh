972f6085cafcd878b395cfae57fcc0808e556991
Kartik Ahuja <kartikahuja@fb.com>
2023-05-26 02:54:07 +0200

    copyright info added

--- lreg_icl2.sh
+++ lreg_icl2.sh
@@ -1,4 +1,4 @@
-## Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
 
 #!/bin/bash
 #SBATCH --cpus-per-task=10
